6de9c266b6770ac302b2119c0d8f8038b03367e0
demipenguin <penguin7272000@gmail.com>
2013-12-15 22:08:10 +0800

    Updated p06 with List.rev.

--- 06.ml
+++ 06.ml
@@ -1,14 +1,7 @@
 (* Find out whether a list is a palindrome. *)
 
 let is_palindrome lst =
-  let rev lst =
-    let rec iter acc = function
-        [] -> acc
-      | hd :: tl -> iter (hd :: acc) tl
-    in
-      iter [] lst
-  in
-    lst = rev lst
+  lst = List.rev lst
 
 let _ = assert(is_palindrome [ "x" ; "a" ; "m" ; "a" ; "x" ] = true)
 let _ = assert(not (is_palindrome [ "a" ; "b" ]) = true)
